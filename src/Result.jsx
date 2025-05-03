827858a9f2598c2557b037c5c876f2abf8d20e61
kingdow10123 <80277412+kingdow10123@users.noreply.github.com>
2025-05-04 03:20:18 +0800

    Create Result.jsx

---
+++ src/Result.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Result() {
+  return (
+    <div className="container">
+      <h1>問卷完成！</h1>
+      <p>感謝你的填寫。</p>
+    </div>
+  );
+}
+
+export default Result;
