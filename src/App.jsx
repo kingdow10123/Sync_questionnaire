3e341494edef720b596d7fcf00f21a1b78c51109
kingdow10123 <80277412+kingdow10123@users.noreply.github.com>
2025-05-04 03:16:50 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import React, { useState } from 'react';
-import SurveyStart from './pages/SurveyStart.jsx';
-import Question1 from './pages/Question1.jsx';
-import Question2 from './pages/Question2.jsx';
-import Question3 from './pages/Question3.jsx';
-import Question4 from './pages/Question4.jsx';
-import Question5 from './pages/Question5.jsx';
-import Question6 from './pages/Question6.jsx';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [page, setPage] = useState('start');
-
   return (
-    <>
-      {page === 'start' && <SurveyStart goPage={setPage} />}
-      {page === 'q1' && <Question1 goPage={setPage} />}
-      {page === 'q2' && <Question2 goPage={setPage} />}
-      {page === 'q3' && <Question3 goPage={setPage} />}
-      {page === 'q4' && <Question4 goPage={setPage} />}
-      {page === 'q5' && <Question5 goPage={setPage} />}
-      {page === 'q6' && <Question6 />}
-    </>
+    <div>
+      <Outlet />
+    </div>
   );
 }
 
